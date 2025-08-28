a9a61cf9dc50495a8b082e15c5cc54d78b0faa8a
FredericNguyen <fredericnguyen@yahoo.com>
2025-08-28 15:39:45 -0400

    rename exercises 1-6_1-14 folder

---
+++ part_1/1-6_1-11-exercises/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const StatisticLine = ({statistic, text}) => {
+    if (text == "positive") {
+        return <tr><td>{text}</td><td>{statistic} %</td></tr>
+    }
+    return <tr><td>{text}</td><td>{statistic}</td></tr>
+}
+
+const Statistics = (props) => {
+    if (props.good + props.neutral + props.bad != 0) {
+        return (
+            <>
+            <h1>statistics</h1>
+            <table>
+                <tbody>
+                    <StatisticLine statistic={props.good} text="good"/>
+                    <StatisticLine statistic={props.neutral} text="neutral"/>
+                    <StatisticLine statistic={props.bad} text="bad"/>
+                    <StatisticLine statistic={props.good + props.neutral + props.bad} text="all"/>
+                    <StatisticLine statistic={(props.good + props.bad*(-1))/(props.good + props.neutral + props.bad)} text="average"/>
+                    <StatisticLine statistic={props.good/(props.good + props.neutral + props.bad)*100} text="positive"/>
+                </tbody>
+            </table>
+            </>
+        )
+    }
+    return (
+        <>
+        <br />
+        <div>No feedback given</div>
+        </>
+    )
+}
+
+const App = () => {
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+  
+    const addGoodFeedback = () => {
+        const updatedGood = good + 1
+        setGood(updatedGood)
+    }
+    const addNeutralFeedback = () => {
+        const updatedNeutral = neutral + 1
+        setNeutral(updatedNeutral)
+    }
+    const addBadFeedback = () => {
+        const updatedBad = bad + 1
+        setBad(updatedBad)
+    }
+    return (
+        <div>
+        <h1>give feedback</h1>
+        <div>
+            <Button onClick={addGoodFeedback} text='good'/>  
+            <Button onClick={addNeutralFeedback} text='neutral'/>
+            <Button onClick={addBadFeedback} text='bad'/>
+        </div>
+        <Statistics good={good} neutral={neutral} bad={bad}/>
+        </div>
+    )
+}
+
+export default App
