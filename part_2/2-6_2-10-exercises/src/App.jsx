e45188325f10e00d4c257b7ce9113746c0d728e3
FredericNguyen <fredericnguyen@yahoo.com>
2026-01-09 10:09:57 -0500

    finished 2.6-2.8

--- part_2/2-6_2-10-exercises/src/App.jsx
+++ part_2/2-6_2-10-exercises/src/App.jsx
@@ -3,24 +3,39 @@ import Person from './components/Person'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', id: "1" }
+        { name: 'Arto Hellas', id: "1", number: '040-123456' }
     ])
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
 
     const addPerson = (event) => {
         event.preventDefault()
-        const personObject = {
-            name: newName,
-            id: String(persons.length + 1),
+        const isDuplicate =
+            persons.some((person) => person.name == newName);
+        if (isDuplicate) {
+            alert(`${newName} is already added to phonebook`)
+        } else {
+            const personObject = {
+                name: newName,
+                id: newName,
+                number: newNumber
+            }
+
+            setPersons(persons.concat(personObject))
+            setNewName('')
+            setNewNumber('')
         }
 
-        setPersons(persons.concat(personObject))
-        setNewName('')
     }
 
     const handleNameChange = (event) => {
         // console.log(event.target.value)
         setNewName(event.target.value)
+    }
+
+    const handleNumberChange = (event) => {
+        // console.log(event.target.value)
+        setNewNumber(event.target.value)
     }
 
     return (
@@ -31,6 +46,12 @@ const App = () => {
                     name: <input
                         value={newName}
                         onChange={handleNameChange}
+                    />
+                </div>
+                <div>
+                    number: <input
+                        value={newNumber}
+                        onChange={handleNumberChange}
                     />
                 </div>
                 <div>
