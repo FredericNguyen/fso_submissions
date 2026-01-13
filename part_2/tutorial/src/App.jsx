cf010bcf7d56725350a68ab1387683f5c1b2d635
FredericNguyen <fredericnguyen@yahoo.com>
2026-01-12 20:15:14 -0500

    2.11 and tutorial done.

--- part_2/tutorial/src/App.jsx
+++ part_2/tutorial/src/App.jsx
@@ -8,16 +8,14 @@ const App = () => {
     const [showAll, setShowAll] = useState(true)
 
     useEffect(() => {
-    console.log('effect')
-    axios.get('http://localhost:3001/notes').then((response) => {
-      console.log('promise fulfilled')
-      setNotes(response.data)
-    })
-  }, [])
-  console.log('render', notes.length, 'notes')
-
-    useEffect(hook, [])
-    console.log('render', notes.length, 'notes')
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/notes')
+            .then(response => {
+                console.log('promise fulfilled')
+                setNotes(response.data)
+            })
+    }, [])
 
     const addNote = (event) => {
         event.preventDefault()
